4e56f0af815081ffaafecf38239c3d907c8d16ee
marcelocampos98 <marcelocampos190098@gamil.com>
2026-01-25 23:20:38 -0400

    commit inicial

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,66 +1,55 @@
-// src/App.tsx
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
-import "./App.css";
+import './App.css';
+import logCriative from './assets/logo.svg';
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [name, setName] = useState("unknown");
-
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-				<a href="https://hono.dev/" target="_blank">
-					<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-				</a>
-				<a href="https://workers.cloudflare.com/" target="_blank">
-					<img
-						src={cloudflareLogo}
-						className="logo cloudflare"
-						alt="Cloudflare logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React + Hono + Cloudflare</h1>
-			<div className="card">
-				<button
-					onClick={() => setCount((count) => count + 1)}
-					aria-label="increment"
-				>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<div className="card">
-				<button
-					onClick={() => {
-						fetch("/api/")
-							.then((res) => res.json() as Promise<{ name: string }>)
-							.then((data) => setName(data.name));
-					}}
-					aria-label="get name"
-				>
-					Name from API is: {name}
-				</button>
-				<p>
-					Edit <code>worker/index.ts</code> to change the name
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p>
-		</>
-	);
+  return (
+    <>
+      <div className="flex justify-center">
+        <img src={logCriative} />
+        <div className="flex flex-col justify-center items-center gap-6">
+          <p>
+            Veja nossos <strong>produtos</strong> e <strong>serviços</strong>{' '}
+            disponíveis
+          </p>
+          <a
+            className="link-button"
+            href="https://criativegraficacg.meloja.com.br/"
+          >
+            CATÁLOGO
+          </a>
+          <p>
+            Faça seu pedido de forma <strong>rápida</strong> e{' '}
+            <strong>fácil</strong> pelo WhatsApp
+          </p>
+          <a
+            className="link-button"
+            href="https://criativegraficacg.meloja.com.br/"
+          >
+            REALIZAR PEDIDO
+          </a>
+          <p>
+            Solicite um <strong>orçamento</strong> abrindo nosso app de{' '}
+            <strong>agendamento</strong>
+          </p>
+          <a
+            className="link-button"
+            href="https://criativegraficacg.meloja.com.br/"
+          >
+            ORÇAMENTO DE FACHADA
+          </a>
+          <p>
+            Agende sua <strong>criação de arte</strong> diretamente em nosso app
+          </p>
+          <a
+            className="link-button"
+            href="https://criativegraficacg.meloja.com.br/"
+          >
+            AGENDAR CRIAÇÃO DE ARTE
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
